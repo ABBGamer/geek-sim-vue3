416077bcf0545f3b772e24eeecdf5f1f3867d7ff
Антон <i@anton-abbg.ru>
2023-11-18 01:11:29 +0700

    add Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,55 @@
-<template>
-  <div class="wrapper">
-    <PageHeader/>
-    <PageContent>
-      <img alt="Vue logo" src="./assets/logo.png">
-    </PageContent>
-    <PageFooter/>
-  </div>
+<!--<template>-->
+<!--  <div class="wrapper">-->
+<!--    <div>-->
 
+<!--    </div>-->
+<!--    <PageHeader/>-->
+<!--    <PageContent>-->
+<!--      <img alt="Vue logo" src="./assets/logo.png">-->
+<!--    </PageContent>-->
+<!--    <PageFooter/>-->
+<!--  </div>-->
+
+<!--</template>-->
+
+<template>
+  <v-app>
+    <v-btn @click="toggleTheme">toggle theme</v-btn>
+
+    <div style="margin-top: 10px">
+      <v-btn>test</v-btn>
+
+    </div>
+    <div style="display: flex;justify-content: center">
+      <div style="margin-top: 10px; width: 50%">
+        <v-combobox
+            label="Combobox"
+            :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
+            variant="solo-inverted"
+        ></v-combobox>
+      </div>
+    </div>
+  </v-app>
 </template>
 
-<script>
+<script setup>
 
-import {defineComponent} from "vue";
-import PageHeader from "@/components/PageHeader.vue";
-import PageFooter from "@/components/PageFooter.vue";
-import PageContent from "@/components/PageContent.vue";
+// import {defineComponent} from "vue";
+// import PageHeader from "@/components/PageHeader.vue";
+// import PageFooter from "@/components/PageFooter.vue";
+// import PageContent from "@/components/PageContent.vue";
 
-export default defineComponent({
-  components: {PageContent, PageFooter, PageHeader}
-})
+import {useTheme} from 'vuetify'
+
+const theme = useTheme()
+
+const toggleTheme = () => {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+
+// export default defineComponent({
+//   components: {PageContent, PageFooter, PageHeader}
+// })
 </script>
 
 <style>
